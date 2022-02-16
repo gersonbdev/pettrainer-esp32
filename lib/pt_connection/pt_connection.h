cc6e5f8182000d9a032dbbfd4a2b3ea5e7491c70
Gerson Benavides <gersonbdev@gmail.com>
2022-02-16 15:09:45 -0500

    File structure improvements

---
+++ lib/pt_connection/pt_connection.h
@@ -0,0 +1,17 @@
+void connectionServer();
+
+void setup_wifi();
+
+void paginaconf();
+
+void modoconf();
+
+void guardar_conf();
+
+void grabar(int addr, String a);
+
+String leer(int addr);
+
+void escanear();
+
+void setupInitialIoT();
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,42 @@
 #include <Arduino.h>
+// Libraries
+#include <RTClib.h> // Real Time Clock module
+#include <EEPROM.h> // EEPROM partition from flash memory
+#include <Wire.h> // I2C
+
+// User Libraries
+#include "pt_connection.h"
+#include "pt_constants.h"
+#include "pt_dispense.h"
+
+static bool power_button_event = false;
+static bool enable = false;
+static short int state = 1;
 
 void setup() {
   // put your setup code here, to run once:
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (power_button_event) {
+    enable = !enable;
+  }
+
+  if (enable) {
+    switch (state) {
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      default:
+        state = 1;
+        break;
+    }
+  }
 }
